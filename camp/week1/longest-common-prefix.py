5b107870fdd9df43960eba50a234701759d77f31
bezawitassaye <119955274+bezawitassaye@users.noreply.github.com>
2023-11-29 19:20:02 +0300

    Added solution for Longest Common Prefix

---
+++ camp/week1/longest-common-prefix.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        prefix = ""
+        for char in zip(*strs):
+            if len(set(char)) == 1:
+                prefix += char[0]
+            else:
+                break
+
+        return prefix
