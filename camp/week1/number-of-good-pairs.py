714640fca2f46a16b90894865123c32182130e9e
bezawitassaye <119955274+bezawitassaye@users.noreply.github.com>
2023-11-30 17:32:51 +0300

    Added solution for Number of Good Pairs

---
+++ camp/week1/number-of-good-pairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        hashset={}
+        count=0
+
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] == nums[j]:
+                    count+=1
+        return count            
+
+
+
+
+
+        
