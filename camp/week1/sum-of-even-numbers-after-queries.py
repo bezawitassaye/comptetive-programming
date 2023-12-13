83ad91fca2d7bf86d18a08c4489d89de4a5b59af
bezawitassaye <119955274+bezawitassaye@users.noreply.github.com>
2023-12-13 13:59:30 +0300

    Added solution for Sum of Even Numbers After Queries

---
+++ camp/week1/sum-of-even-numbers-after-queries.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        answer = []
+        even_sum = sum(num for num in nums if num % 2 == 0)
+
+        for val, index in queries:
+            if nums[index] % 2 == 0:
+                even_sum -= nums[index]
+            
+            nums[index] += val
+
+            if nums[index] % 2 == 0:
+                even_sum += nums[index]
+            
+            answer.append(even_sum)
+        
+        return answer
