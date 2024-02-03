8ff873e2bfb1b3d5205229876f301aa9e9110264
bezawitassaye <119955274+bezawitassaye@users.noreply.github.com>
2024-02-03 20:06:32 +0300

    Added solution for Move Zeroes

--- camp/week1/move-zeroes.py
+++ camp/week1/move-zeroes.py
@@ -1,10 +1,16 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        start=0
+        l=0
+        r=1
+        while r < len(nums):
+            if nums[l] ==0 and nums[r] !=0:
+                nums[l] ,nums[r] = nums[r],nums[l]
+                l+=1
+                r+=1
+            elif nums[l] == 0 and nums[r]==0:
+                r+=1  
+            else:
+                l+=1
+                r+=1      
 
-        for i in range(len(nums)):
-            if nums[i]:
-                nums[start],nums[i]=nums[i], nums[start]
-                start+=1
-         
-             
+        
