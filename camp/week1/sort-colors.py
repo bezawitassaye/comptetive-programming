63c6a6f4e125b1272a057cf3e603a37fc38a2b5f
bezawitassaye <119955274+bezawitassaye@users.noreply.github.com>
2024-02-04 14:41:05 +0300

    Added solution for Sort Colors

--- camp/week1/sort-colors.py
+++ camp/week1/sort-colors.py
@@ -1,4 +1,24 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         nums.sort()
-        
+        r=1
+        color=0
+        count=0
+        l=0
+        while r < len(nums):
+            if nums[l] != color:
+                if nums[r] == color:
+                    nums[l],nums[r] = nums[r],nums[l]
+                    count+=1
+                    l+=1
+                    r+=l
+                elif nums[r] == color:
+                     r+=l    
+                elif count==2:
+                    color+=1
+                    count=0
+            
+            l+=1
+            r+=1
+
+
