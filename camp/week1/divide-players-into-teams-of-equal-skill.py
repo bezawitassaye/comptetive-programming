6056602a7ae060d2744c96615aec39610b4e6223
bezawitassaye <119955274+bezawitassaye@users.noreply.github.com>
2024-02-05 12:18:16 +0300

    Added solution for Divide Players Into Teams of Equal Skill

--- camp/week1/divide-players-into-teams-of-equal-skill.py
+++ camp/week1/divide-players-into-teams-of-equal-skill.py
@@ -1,32 +1,21 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
-    
         skill.sort()
+        k=sum(skill)*2//len(skill)
+        pro=0
         l=0
-        check=0
         r=len(skill)-1
-        hashset={}
-        hashset2={}
-        count=0
-        if len(skill) == 2:
-            return skill[0]*skill[1]
-        else:
-            while l < r:
-                hashset[count]=skill[l]*skill[r]
-                hashset2[count]=skill[l]+skill[r]
-                count+=1
-                l+=1
-                r-=1
-        if len(set(hashset2.values())) == 1:
-            for value in hashset.values():
-                check+=value
-            
-            return check   
-        else:
-            return -1    
-
-
-
+        while l<r:
+            if skill[l]+skill[r] ==k:
+                count=skill[l]*skill[r]
+                pro+=count
+            else:
+                return -1
+            l+=1
+            r-=1
+        return pro            
+    
+        
      
 
             
