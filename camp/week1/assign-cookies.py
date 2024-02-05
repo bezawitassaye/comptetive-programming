7b9e41eb885b3e2696ffd17fc33db07f034181c0
bezawitassaye <119955274+bezawitassaye@users.noreply.github.com>
2024-02-05 12:18:25 +0300

    Added solution for Assign Cookies

--- camp/week1/assign-cookies.py
+++ camp/week1/assign-cookies.py
@@ -1,14 +1,22 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        l=0
+        r=0
         g.sort()
         s.sort()
-        greed=0
-        cookie=0
-
-        while greed < len(g) and cookie < len(s):
-            if s[cookie] >= g[greed]:
-                greed+=1
-            cookie+=1
-        return greed        
-
+        count=0
+        if len(s) ==0:
+            return 0
+        while l < len(g) and r<len(s) :
+            
+            while r< len(s):
+                if g[l]<=s[r]:
+                    print(g[l],s[r])
+                    count+=1
+                    r+=1
+                    break
+                r+=1    
+            l+=1
+           
+        return count        
         
