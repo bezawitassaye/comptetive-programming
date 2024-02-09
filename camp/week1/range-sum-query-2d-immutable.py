3586afb6af33180673bd0fbdafcd6cdc5a6827a3
bezawitassaye <119955274+bezawitassaye@users.noreply.github.com>
2024-02-09 21:52:27 +0300

    Added solution for Range Sum Query 2D - Immutable

--- camp/week1/range-sum-query-2d-immutable.py
+++ camp/week1/range-sum-query-2d-immutable.py
@@ -1,25 +1,21 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        rowLength, colLength = len(matrix), len(matrix[0])
-        self.sumMat = [[0] * (colLength + 1) for rows in range(rowLength + 1)]
-        
-        for row in range(rowLength):
-            prefixSum = 0
-            for col in range(colLength):
-                prefixSum += matrix[row][col]
-                above = self.sumMat[row][col + 1]
-                self.sumMat[row + 1][col + 1] = prefixSum + above                
-                
-
-    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:        
-        row1, col1, row2, col2 = row1+1, col1+1, row2+1, col2+1
-        bottomRight = self.sumMat[row2][col2]
-        above = self.sumMat[row1 - 1][col2]
-        left = self.sumMat[row2][col1 - 1]
-        topLeft = self.sumMat[row1 - 1][col1 - 1]
-        
-        return bottomRight - above - left + topLeft 
+        r = len(matrix) 
+        c = len(matrix[0])
+        self.new = [[0] * (c+1) for _ in range(r+1)]
+        for row in range(r):
+            pref = 0
+            for col in range(c):
+                pref += matrix[row][col]
+                above = self.new[row][col + 1]
+               
+                self.new[row + 1][col + 1] = pref + above
+    
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int: 
+        row1,row2,col1,col2=row1+1,row2+1,col1+1,col2+1
+        return    self.new[row2][col2]-self.new[row1-1][col2]-self.new[row2][col1-1] +self.new[row1-1][col1-1]  
+       
         
 
 
