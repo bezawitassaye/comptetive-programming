95def1163a8bbfe9b1f8868b5966e895c946f48b
bezawitassaye <119955274+bezawitassaye@users.noreply.github.com>
2024-02-23 21:13:53 +0300

    Add solution for Two City Scheduling

--- leetcode/two-city-scheduling.py
+++ leetcode/two-city-scheduling.py
@@ -7,11 +7,11 @@ class Solution:
         arr.sort()
         print(arr)
         count=0
+        n=len(arr)//2
         for i in range(len(arr)):
-            if i < len(arr)//2:
-             
+            if i < n:
                 count+=arr[i][1][0]
-            elif i >= len(arr)//2:
+            elif i >= n:
                
                 count+=arr[i][1][1]
      
