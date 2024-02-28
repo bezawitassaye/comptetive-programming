1fa0053cb47d31b744ac15111efc9efcced9c14a
bezawitassaye <119955274+bezawitassaye@users.noreply.github.com>
2024-02-28 23:32:00 +0300

    Add solution for Rabbits in Forest

--- leetcode/rabbits-in-forest.py
+++ leetcode/rabbits-in-forest.py
@@ -5,12 +5,9 @@ class Solution:
    
         for i in answers:
             hashset[i]=1+hashset.get( i,0)  
+        print(hashset)    
         for  j in hashset:
-            if hashset[j] <= j:
-                count+=j+1
-            elif j==0:
-                count+=hashset[j]
-            elif hashset[j] > j:
+            
               count+=hashset[j]- (hashset[j] % (j+1))
               if (hashset[j] % (j+1)):
                   count+=j+1
