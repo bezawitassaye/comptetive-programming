741d2cdb2f68010c1d3ef4d1619cf498b71267d7
bezawitassaye <119955274+bezawitassaye@users.noreply.github.com>
2024-03-09 09:30:08 +0300

    Add solution for Maximum Width of Binary Tree

---
+++ leetcode/maximum-width-of-binary-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res=0
+        q=deque([[root,1,0]])
+        prevlevel,prevnum=0,1
+        while q: 
+            node , num,level = q.popleft()
+            if level > prevlevel:
+                prevlevel=level
+                prevnum=num
+            res=max(res,num - prevnum+1)
+            if node.left:
+                q.append([node.left,num*2,level+1])
+            if node.right:
+                q.append([node.right,num*2+1,level+1])
+        return res                  
